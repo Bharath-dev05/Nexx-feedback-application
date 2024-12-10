a448811ff9a05bd7bd7384042ef5c4788b9161dc
Bharath <bharathxps05@gmail.com>
2024-12-10 22:41:47 +0530

    update next version

--- app/(user)/payments/subscribe/page.tsx
+++ app/(user)/payments/subscribe/page.tsx
@@ -1,50 +1,23 @@
-// import SubscribeBtn from "../subscribe-btn";
-// import { monthlyPlanId,yearlyPlanId } from "@/lib/payments";
-
-// interface PageProps { searchParams?: { plan?: string; }; }
-
-
-// const page = async  ({ searchParams }: { searchParams?: { plan?: string } }) =>{
-//   const resolvedSearchParams = await Promise.resolve(searchParams || {});
-
-//   const plan = resolvedSearchParams?.plan || "monthly";
-//     const planId= plan === "monthly" ?monthlyPlanId:yearlyPlanId
-//     return(
-//         <div className="flex border p-4 rounded-md flex-col">
-
-//           <div className="w-fit mt-3">
-//             <h1 className="text-2xl font-bold">Start your subscription now:</h1>
-//           <SubscribeBtn price={planId}/>
-//           </div>
-//         </div>
-//     )
-    
-// }
-
-// export default page;
-
-
 import SubscribeBtn from "../subscribe-btn";
 import { monthlyPlanId, yearlyPlanId } from "@/lib/payments";
 
-interface PageProps {
-  searchParams?: {
-    plan?: string|undefined;
-  };
-}
+const page = ({ searchParams }: {
+  searchParams: {
+    plan: string
+  }
+}) => {
+  const { plan } = searchParams;
 
-const Page = ({ searchParams }: PageProps) => {
-  const { plan } = searchParams ?? {};
   const planId = plan === "monthly" ? monthlyPlanId : yearlyPlanId;
 
   return (
     <div className="flex border p-4 rounded-md flex-col">
+      <h1 className="text-2xl font-bold">Start your subscription now:</h1>
       <div className="w-fit mt-3">
-        <h1 className="text-2xl font-bold">Start your subscription now:</h1>
         <SubscribeBtn price={planId} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page;
