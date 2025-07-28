59ddf45691e8d6590b9be8e27b5fd45a2f1f1ce6
Bharath-dev05 <bharathsde05@gmail.com>
2025-07-28 18:07:18 +0530

    Refactor subscribe page to use searchParams
    Updated the subscribe page to extract the plan from searchParams instead of awaiting params. Simplified the component to a functional component and cleaned up unused props.

--- app/(user)/payments/subscribe/page.tsx
+++ app/(user)/payments/subscribe/page.tsx
@@ -1,16 +1,11 @@
 import SubscribeBtn from "../subscribe-btn";
 import { monthlyPlanId, yearlyPlanId } from "@/lib/payments";
 
-import { ReactNode } from "react";
 
-export default async function page(props: { children: ReactNode; params: Promise<{ plan: string }> }) {
-  const { children, params } = props;
-  
-  // Extract the plan from params
-  const { plan } = await params;
-
+const page = ({ searchParams }: { searchParams: { plan: string }}) => {
+  const { plan } = searchParams;
   const planId = plan === "monthly" ? monthlyPlanId : yearlyPlanId;
-
+  
   return (
     <div className="flex border p-4 rounded-md flex-col">
       <h1 className="text-2xl font-bold">Start your subscription now:</h1>
@@ -21,4 +16,6 @@ export default async function page(props: { children: ReactNode; params: Promise
   )
 }
 
-// export default page;
+export default page;
+
+
